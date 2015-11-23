49d366ce6f80b2ff87e574a4d750e695d0a65541
SugiKent <ynmk1995@gmail.com>
2015-11-23 09:26:21 +0900

    途中経過

---
+++ jquery.js
@@ -0,0 +1,49 @@
+//メニューバーのスライド表示
+$(function() {
+$('header').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+	if (isInView) {
+		if (visiblePartY == 'both') {
+			$(this).stop().show(1500);
+		}
+	}
+});
+});
+
+//index.htmlのロゴアニメーション
+$(function() {
+$('.top-logo').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+	if (isInView) {
+		if (visiblePartY == 'both') {
+			$(this).stop().fadeIn(1200);
+		}
+	}
+});
+});
+
+//index.htmlのタイプライターアニメ
+$(function(){
+    var setElm = $('.split'),
+    delaySpeed = 60,
+    fadeSpeed = 650;
+
+    setText = setElm.html();
+
+    setElm.css({visibility:'visible'}).children().addBack().contents().each(function(){
+        var elmThis = $(this);
+        if (this.nodeType == 3) {
+            var $this = $(this);
+            $this.replaceWith($this.text().replace(/(\S)/g, '<span class="textSplitLoad">$&</span>'));
+        }
+    });
+    $(window).load(function(){
+        splitLength = $('.textSplitLoad').length;
+        setElm.find('.textSplitLoad').each(function(i){
+            splitThis = $(this);
+            splitTxt = splitThis.text();
+            splitThis.delay(i*(delaySpeed)).css({display:'inline-block',opacity:'0'}).animate({opacity:'1'},fadeSpeed);
+        });
+        setTimeout(function(){
+                setElm.html(setText);
+        },splitLength*delaySpeed+fadeSpeed);
+    });
+});
