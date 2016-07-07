393ff661f63914e246a445af887392e7d6dd73a0
SugiKent <ynmk1995@gmail.com>
2016-07-07 15:37:04 +0900

    memberページのfirst seminarの更新

--- jquery.js
+++ jquery.js
@@ -1,5 +1,9 @@
-//メニューバーのスライド表示
+$(window).ready(function(){
+	$('.member-content__profile p').next().hide();
+});
 $(function() {
+
+	//メニューバーのスライド表示
 $('header').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
 	if (isInView) {
 		if (visiblePartY == 'both') {
@@ -7,50 +11,56 @@ $('header').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
 		}
 	}
 });
-});
-$(function() {
-//スマホメニュー部分のアニメーション
-  $('#language-show').click(function() {
-    $('#language-list').toggleClass('hide-list').next().slideToggle(1000);
-  });
-});
 
+	// Memberページのプロフィール名前
+	$('.member-content__profile p').hover(function(){
+		name = $(this).text();
+		// fblink = $(this + 'a').attr('href');
+		var position = $($(this).next()).text();
+		$(this).text(position);
+	},
+	function(){
+		$(this).html(name);
+	});
 
-//index.htmlのロゴアニメーション
-$(function() {
-$('.top-logo').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
-	if (isInView) {
-		if (visiblePartY == 'both') {
-			$(this).stop().fadeIn(1200);
+	//スマホメニュー部分のアニメーション
+	  $('#language-show').click(function() {
+	    $('#language-list').toggleClass('hide-list').next().slideToggle(1000);
+	  });
+
+		//index.htmlのロゴアニメーション
+	$('.top-logo').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+		if (isInView) {
+			if (visiblePartY == 'both') {
+				$(this).stop().fadeIn(1200);
+			}
 		}
-	}
-});
-});
+	});
 
-//index.htmlのタイプライターアニメ
-$(function(){
-    var setElm = $('.split'),
-    delaySpeed = 60,
-    fadeSpeed = 650;
+	//index.htmlのタイプライターアニメ
+	var setElm = $('.split'),
+	delaySpeed = 60,
+	fadeSpeed = 650;
 
-    setText = setElm.html();
+	setText = setElm.html();
 
-    setElm.css({visibility:'visible'}).children().addBack().contents().each(function(){
-        var elmThis = $(this);
-        if (this.nodeType == 3) {
-            var $this = $(this);
-            $this.replaceWith($this.text().replace(/(\S)/g, '<span class="textSplitLoad">$&</span>'));
-        }
-    });
-    $(window).load(function(){
-        splitLength = $('.textSplitLoad').length;
-        setElm.find('.textSplitLoad').each(function(i){
-            splitThis = $(this);
-            splitTxt = splitThis.text();
-            splitThis.delay(i*(delaySpeed)).css({display:'inline',opacity:'0'}).animate({opacity:'1'},fadeSpeed);
-        });
-        setTimeout(function(){
-                setElm.html(setText);
-        },splitLength*delaySpeed+fadeSpeed);
-    });
+	setElm.css({visibility:'visible'}).children().addBack().contents().each(function(){
+			var elmThis = $(this);
+			if (this.nodeType == 3) {
+					var $this = $(this);
+					$this.replaceWith($this.text().replace(/(\S)/g, '<span class="textSplitLoad">$&</span>'));
+			}
+	});
+	$(window).load(function(){
+			splitLength = $('.textSplitLoad').length;
+			setElm.find('.textSplitLoad').each(function(i){
+					splitThis = $(this);
+					splitTxt = splitThis.text();
+					splitThis.delay(i*(delaySpeed)).css({display:'inline',opacity:'0'}).animate({opacity:'1'},fadeSpeed);
+			});
+			setTimeout(function(){
+							setElm.html(setText);
+			},splitLength*delaySpeed+fadeSpeed);
+	});
+
 });
